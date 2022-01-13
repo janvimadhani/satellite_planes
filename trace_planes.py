515330396a030d2415a36684c4d35c5f01d51e15
Janvi Madhani <janvimadhani@gmail.com>
2022-01-13 13:37:03 -0500

    trace systems

---
+++ trace_planes.py
@@ -0,0 +1,60 @@
+import numpy as np
+import time as time
+import plane_finding_tools as pf 
+import sys
+
+snapshot = sys.argv[1]
+
+
+systems_file='/data78/welker/madhani/systems/traced_systems_' + str(snapshot) + '.pickle'
+#systems_file = '/Users/JanviMadhani/satellite_planes/systems/MWsystems.pickle'
+
+systems = pf.read_systems(systems_file)
+
+
+# JUST CHECK WITH ONE RIGHT NOW
+#syst = 46
+for syst in range(len(systems)):
+ 
+    print('System with Halo ID:', systems[syst]['halo_ID'])
+    name_of_syst = systems[syst]['halo_ID']
+    
+
+    best_u1,best_u2,best_u3,best_rms = pf.evolutionary_plane_finder(systems=systems,system=syst,n_iter=200,n_start=25,n_erase=10,n_avg_mutants=5,level=1,rand=False,verbose=True)
+    z_best,xx,yy,unit_n,los = pf.get_plane(u1=best_u1,u2=best_u2,u3=best_u3,systems=systems,system=syst)
+
+    #get physical extent, c_to_a:
+    a,b,c,phys_c_to_a = pf.find_physical_extent(u1=best_u1,u2=best_u2,u3=best_u3,systems=systems,system=syst,actual_rms=best_rms,nrms = 2,level=1)
+    phys_ext = [a,b,c,phys_c_to_a]
+
+    #find inertia tensor
+    I = pf.find_inertia_tensor(systems[syst])
+    v1,v2,v3 = pf.find_axes_of_rot(I)
+    i_c_to_a = pf.find_axes_ratios(I)
+
+    inertia = [v1,v2,v3,i_c_to_a]
+
+    name_of_3dplot = 'system_' + str(name_of_syst) +'.png'
+    pf.save_3Dplot(name_of_3dplot,systems=systems,syst=syst,snapshot=snapshot,xx=xx,yy=yy,z_best=z_best,los=los,unit_n=unit_n,phys_ext = phys_ext, inertia=inertia)
+
+
+
+    #check for isotropy n times and find n rms dists
+    #iso_sph_systs_rms,iso_ell_systs_rms = pf.check_isotropy(systems=systems,syst=syst,n=2000)
+
+    #name_of_hist = 'system_' + str(name_of_syst) +'_hist.png'
+    
+    #save spherical and get significance 
+    #sph_sig = pf.save_hist(name_of_hist,best_rms,iso_sph_systs_rms,snapshot=snapshot,type='spherical')
+
+    #save elliptical and get significance 
+    #ell_sig = pf.save_hist(name_of_hist,best_rms,iso_ell_systs_rms,snapshot=snapshot,type='elliptical')
+
+
+    ## find significance of rms then change below file to include this info
+
+
+    #save all information to a .csv file
+    #name_of_file = 'system_' + str(name_of_syst) + '.csv'
+    #pf.save_outputs(name_of_file,snapshot=snapshot,systems=systems,syst=syst,inertial=inertia,physical=phys_ext,sig_spherical=sph_sig,sig_elliptical=ell_sig)
+
---
+++ trace_systems.py
@@ -0,0 +1,42 @@
+import numpy as np
+import fortran_reader as fr
+import analysis_tools as analyze
+import time as time
+import sys
+#from analysis_tools import MWsystems as MWS
+
+t0 = time.time()
+
+#CHANGE THIS TO BE THE SNAPSHOT YOU ARE ANALYZING -- 970 IS THE ONE I'VE WORKED WITH MOST
+#when you run this file, run it as python3 find_systems.py 970 
+snapshot = sys.argv[1]
+
+#LOCAL PATHS
+#filepath_haloes = '/Users/JanviMadhani/satellite_planes/catalogs/tree_bricks'+str(snapshot)
+#filepath_galaxies = '/Users/JanviMadhani/satellite_planes/catalogs/tree_bricks' +str(snapshot) +'_stars_NH'
+
+#INFINITY PATHS
+filepath_haloes = '/home/madhani/satellite_planes/catalogs/Halos/TREE_DM/tree_bricks'+str(snapshot)
+filepath_galaxies = '/home/madhani/satellite_planes/catalogs/Stars/TREE_STARS_HOP_dp_SCnew_gross/tree_brick_'+str(snapshot)
+
+
+haloes = fr.ReadTreebrick_lowp(filepath_haloes)
+galaxies = fr.GalaxyCatalog(filepath_galaxies)
+
+haloes_dict = haloes.treebricks_dict
+galaxies_dict = galaxies.treebricks_dict
+
+systs = analyze.MWsystems(haloes_dict,galaxies_dict) #first make a class object of all systems
+
+#the halos you are trying to trace
+haloIDs = [1301,10889,1421,563,1388,563,3331,5485,675,5498,10886,8292,467]
+
+halosystems = systs.find_systs_by_halo_ids(haloes_dict,galaxies_dict) #then find systems corresponding to these halos
+
+#write file 
+print('Writing Halo Systems to file...')
+systs.write_to_pickle('traced_systems_'+str(snapshot),rewrite=True)
+t1 = time.time()
+
+
+print('Total time taken:', t1-t0, 's')
