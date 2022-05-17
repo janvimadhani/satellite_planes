c60192eb9b06457e93aac92909ba43cc7bbc7c4e
Janvi Madhani <janvimadhani@gmail.com>
2022-05-17 16:01:06 -0400

    typo

--- get_net.f90
+++ get_net.f90
@@ -22,7 +22,7 @@ character(len=4) ::opt
 integer(kind=4) :: i,j,k
 character(len=300) :: inputfile, output_dir, outputfile, testfile
 integer(kind=4) :: snap
-character(len=10) :: snap0
+character(len=4) :: snap0
 integer::narg
 
 
